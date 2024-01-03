9269f9a6c4d832cb7c934e1d932c168f4c4b17c0
CosmeNicolas <nicomas7590@gmail.com>
2024-01-03 15:23:15 -0300

    cambie el color del titulo

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
      <Container fluid className="main">
-      <h1 className='color text-center mt-5'>Color List</h1>
+        <h1 className='color text-center mt-5'>🖌️Color List 🎨</h1>
      </Container>
      <Footer/>
     </>
