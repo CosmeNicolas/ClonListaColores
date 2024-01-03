850f531ed96c356b7e4227ad61df403395c588ae
CosmeNicolas <nicomas7590@gmail.com>
2024-01-03 14:59:02 -0300

    creamos el maquetado del proyecto e instale las dependecias necesarias como react-bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <Container fluid>
+      <h1>Color List</h1>
+     </Container>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
+  );
+};
+
+export default Footer;
